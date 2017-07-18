aaa514b3a8edce1cc1961548dcc501f1bd16ed86
David T. Weichselbaum <david.t.weichselbaum@gmail.com>
2017-07-18 10:15:48 +0200

    Add example image display

--- font-to-png.sh
+++ font-to-png.sh
@@ -2,13 +2,18 @@
 
 fontdir='/usr/share/fonts'
 imagedir='./fontImages'
+tabledir='./fontTables'
 
-fonts=$(find "$fontdir"/* -name '*ttf')
+fonts=$(find "$fontdir"/* -name '*ttf' | sort)
 
 for font in $fonts; do
 	fontname=$(basename $font)
 	echo $fontname
+	fontTable=$tabledir'/'$fontname'.ttx'
+	ttx -q -t cmap -o $fontTable $font
 	for letter in {A..Z}; do
+		exists=$(grep 'name="'$letter'"' $fontTable) # check if letter exists in that font
+		if [ -z "$exists" ]; then continue; fi # if not skip font
 		filename=$imagedir'/'$fontname'_'$letter.png
         	convert -background white -fill black -font $font -pointsize 300 label:"$letter" $filename
         	convert $filename -resize 64x128! $filename
--- fontFun.py
+++ fontFun.py
@@ -1,6 +1,29 @@
 #!/usr/bin/python3
 
 imgDir='fontImages'
+valSplit = 0.1
+
+# utility function for showing images
+def show_imgs(x_test, decoded_imgs=None, n=10):
+	import matplotlib.pyplot as plt
+	from keras.preprocessing import image
+	plt.figure(figsize=(20, 4))
+	for i in range(n):
+		ax = plt.subplot(2, n, i+1)
+		img = image.array_to_img(x_test[i])
+		plt.imshow(img)
+		plt.gray()
+		ax.get_xaxis().set_visible(False)
+		ax.get_yaxis().set_visible(False)
+
+		if decoded_imgs is not None:
+			ax = plt.subplot(2, n, i+ 1 +n)
+			img = image.array_to_img(decoded_imgs[i])
+			plt.imshow(img)
+			plt.gray()
+			ax.get_xaxis().set_visible(False)
+			ax.get_yaxis().set_visible(False)
+	plt.show()
 
 import os
 import collections
@@ -12,28 +35,40 @@ for file in os.listdir(imgDir):
 		fileName = os.path.basename(path)
 		fontName = fileName[:-10]
 		letter = fileName[-5:-4]
-		img = image.load_img(path)
+		img = image.load_img(path, grayscale=True)
 		img = image.img_to_array(img)
 		fonts[fontName][letter] = img
 
 As, Bs = [], []
 for fontName, letters in fonts.items():
-	As.append( letters['A']	)
-	Bs.append( letters['B']	)
+	try:
+		A = letters['A']
+		B = letters['B']
+		As.append(A)
+		Bs.append(B)
+	except: pass
 import numpy as np
 As, Bs = np.array(As), np.array(Bs)
-As = As.astype('float32')/255. 
-Bs = Bs.astype('float32')/255. 
-print('shapes: ', As.shape, Bs.shape)
-AsShape, BsShape = As[0].shape, Bs[0].shape
+As = As.astype('float32')/255.
+Bs = Bs.astype('float32')/255.
+nData = len(As)
+perm = np.random.permutation(nData)
+As, Bs, = As[perm], Bs[perm]
+nVal = int(nData * valSplit)
+
+AsVal, BsVal = As[:nVal], Bs[:nVal]
+AsTrain, BsTrain = As[nVal:], Bs[nVal:]
+print('shapes: ', AsTrain.shape, BsVal.shape)
+AsShape, BsShape = AsTrain[0].shape, BsTrain[0].shape
 		
-from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, UpSampling2D, BatchNormalization
 from keras.models import Model
 from keras import backend as K
 
 input_img = Input(shape=AsShape) # 1ch=black&white, 28 x 28
 
-x = Convolution2D(16, (3, 3), activation='relu', padding='same')(input_img) #nb_filter, nb_row, nb_col
+# x = BatchNormalization()(input_img)
+x = Convolution2D(16, (3, 3), activation='relu', padding='same')(input_img)
 x = MaxPooling2D((2, 2), padding='same')(x)
 x = Convolution2D(8, (3, 3), activation='relu', padding='same')(x)
 x = MaxPooling2D((2, 2), padding='same')(x)
@@ -47,15 +82,20 @@ x = Convolution2D(8, (3, 3), activation='relu', padding='same')(x)
 x = UpSampling2D((2, 2))(x)
 x = Convolution2D(16, (3, 3), activation='relu', padding='same')(x) 
 x = UpSampling2D((2, 2))(x)
-decoded = Convolution2D(3, (5, 5), activation='sigmoid', padding='same')(x)
+decoded = Convolution2D(1, (5, 5), activation='sigmoid', padding='same')(x)
 print("shape of decoded", K.int_shape(decoded))
 
 autoencoder = Model(input_img, decoded)
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 print(autoencoder.summary())
 
-from keras.callbacks import TensorBoard
+from keras.callbacks import TensorBoard, EarlyStopping
+import datetime
+dt = datetime.datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+callbacks=[TensorBoard(log_dir='./Logs/%s' % (dt)), EarlyStopping(patience=5)]
 
-autoencoder.fit(As, Bs, epoch=50, batch_size=128,
-		shuffle=True, validation_split=0.1, verbose=1,
-		callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+autoencoder.fit(AsTrain, BsTrain, epochs=1000, batch_size=1,
+		shuffle=True, validation_data=(AsVal, BsVal), verbose=1,
+		callbacks=[TensorBoard(log_dir='./Logs/%s' % (dt))])
+BsPred = autoencoder.predict(AsVal)
+show_imgs(AsVal, BsPred)
